1ab3725417c1c07d073d4149b65b0900f90178fa
ROgbuagu05 <chizzy3003@gmail.com>
2024-01-31 08:43:27 -0800

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,21 +11,27 @@ def validUTF8(data):
     Returns:
     True if the data is valid UTF-8, False otherwise.
     """
-    num_bytes = 0
+    n_bytes = 0
 
     for byte in data:
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
+        bin_rep = format(byte, '#010b')[-8:]
 
-    return num_bytes == 0
+        if n_bytes == 0:
+
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+        n_bytes -= 1
+
+    return n_bytes == 0
